14b5aefbabb23dd982ed38dffd6932296dc2007b
Bright <johnbright081132@gmail.com>
2025-11-13 22:05:40 +0100

    update index.html to recieve FCM Tokens

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAvu7XOTcoexHqS9o11uTfQRPyMjJE_z8U",
+    authDomain: "push-service-184a8.firebaseapp.com",
+    projectId: "push-service-184a8",
+    storageBucket: "push-service-184a8.firebasestorage.app",
+    messagingSenderId: "1096856520461",
+    appId: "1:1096856520461:web:dd1d3fb6c3ea018e3a5244"
+});
+
+const messaging = firebase.messaging();
